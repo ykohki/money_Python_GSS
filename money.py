5b622385b7606e0c35aa9476ef2c85d714ba5b9f
ykohki <inaeva7691@gmail.com>
2019-12-22 19:34:53 +0900

    inital commit

--- money.py
+++ money.py
@@ -47,7 +47,23 @@ def handle_message(event):
     scope = ['https://spreadsheets.google.com/feeds',
             'https://www.googleapis.com/auth/drive']
 
-    credentials = ServiceAccountCredentials.from_json_keyfile_name('money-line-python-638e48c935e8.json', scope)
+    #credentials = ServiceAccountCredentials.from_json_keyfile_name('money-line-python-638e48c935e8.json', scope)
+    #認証情報を環境変数から
+    # 辞書オブジェクト。認証に必要な情報をHerokuの環境変数から呼び出している
+    credential = {
+                    "type": "service_account",
+                    "project_id": os.environ['SHEET_PROJECT_ID'],
+                    "private_key_id": os.environ['SHEET_PRIVATE_KEY_ID'],
+                    "private_key": os.environ['SHEET_PRIVATE_KEY'],
+                    "client_email": os.environ['SHEET_CLIENT_EMAIL'],
+                    "client_id": os.environ['SHEET_CLIENT_ID'],
+                    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+                    "token_uri": "https://oauth2.googleapis.com/token",
+                    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+                    "client_x509_cert_url":  os.environ['SHEET_CLIENT_X509_CERT_URL']
+             }
+    credentials = ServiceAccountCredentials.from_json_keyfile_dict(credential, scope)
+    
     gc = gspread.authorize(credentials)
     text = event.message.text
 
